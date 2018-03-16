8c01a1ee9d6e39804cf3db1a3e26be5c99f81dfd
sxiuchen <xiuchen@ualberta.ca>
2018-03-16 14:23:19 -0600

    Update TweetsFileManager.java

--- app/src/main/java/ca/ualberta/cs/lonelytwitter/TweetsFileManager.java
+++ app/src/main/java/ca/ualberta/cs/lonelytwitter/TweetsFileManager.java
@@ -14,6 +14,7 @@ import android.util.Log;
 
 public class TweetsFileManager {
 
+	public static final String FILE_NAME = "file.sav";
 	private Context ctx;
 
 	public TweetsFileManager(Context ctx) {
@@ -25,7 +26,7 @@ public class TweetsFileManager {
 		List<NormalLonelyTweet> tweets = new ArrayList<NormalLonelyTweet>();
 
 		try {
-			FileInputStream fis = ctx.openFileInput("file.sav");
+			FileInputStream fis = ctx.openFileInput(FILE_NAME);
 			ObjectInputStream ois = new ObjectInputStream(fis);
 
 			Object o = ois.readObject();
@@ -49,7 +50,7 @@ public class TweetsFileManager {
 
 	public void saveTweets(List<NormalLonelyTweet> tweets) {
 		try {
-			FileOutputStream fos = ctx.openFileOutput("file.sav", 0);
+			FileOutputStream fos = ctx.openFileOutput(FILE_NAME, 0);
 			ObjectOutputStream oos = new ObjectOutputStream(fos);
 
 			oos.writeObject(tweets);
